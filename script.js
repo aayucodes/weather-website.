ecf748623da25ee038deb8307a682317f6df35db
aayu <aayu.x8@gmail.com>
2026-07-31 22:57:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const locationBtn = document.querySelector(".location-btn");
 const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
@@ -17,22 +18,27 @@ async function checkWeather(city) {
 
     const data = await response.json();
 
-    console.log(data);
-
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    document.querySelector(".temp").innerHTML =
+        Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML =
+        data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML =
+        data.wind.speed + " km/h";
 
     if (data.weather[0].main == "Clouds") {
-        weatherIcon.src = "images/clouds.png";
-    } else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "images/cloud.png";
+    }
+    else if (data.weather[0].main == "Clear") {
         weatherIcon.src = "images/clear.png";
-    } else if (data.weather[0].main == "Rain") {
+    }
+    else if (data.weather[0].main == "Rain") {
         weatherIcon.src = "images/rain.png";
-    } else if (data.weather[0].main == "Drizzle") {
+    }
+    else if (data.weather[0].main == "Drizzle") {
         weatherIcon.src = "images/drizzle.png";
-    } else if (data.weather[0].main == "Mist") {
+    }
+    else if (data.weather[0].main == "Mist") {
         weatherIcon.src = "images/mist.png";
     }
 
@@ -44,11 +50,61 @@ searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
 });
 
-searchBox.addEventListener("keypress", (event) => {
+searchBox.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
         checkWeather(searchBox.value);
     }
 });
 
-// Default city
+locationBtn.addEventListener("click", () => {
+
+    if (navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition(async (position) => {
+
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`
+            );
+
+            const data = await response.json();
+
+            document.querySelector(".city").innerHTML = data.name;
+            document.querySelector(".temp").innerHTML =
+                Math.round(data.main.temp) + "°C";
+            document.querySelector(".humidity").innerHTML =
+                data.main.humidity + "%";
+            document.querySelector(".wind").innerHTML =
+                data.wind.speed + " km/h";
+
+            if (data.weather[0].main == "Clouds") {
+                weatherIcon.src = "images/cloud.png";
+            }
+            else if (data.weather[0].main == "Clear") {
+                weatherIcon.src = "images/clear.png";
+            }
+            else if (data.weather[0].main == "Rain") {
+                weatherIcon.src = "images/rain.png";
+            }
+            else if (data.weather[0].main == "Drizzle") {
+                weatherIcon.src = "images/drizzle.png";
+            }
+            else if (data.weather[0].main == "Mist") {
+                weatherIcon.src = "images/mist.png";
+            }
+
+            document.querySelector(".weather").style.display = "block";
+            document.querySelector(".error").style.display = "none";
+
+        });
+
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+
+});
+
+// Default City
 checkWeather("Vadodara");
